4b4118d8af6b87ebfa2e9d2c02cc5ee750aa3ff5
romaric-g <romaric.gauzi1@gmail.com>
2020-11-09 08:54:19 +0100

    Player modal

--- src/pages/MusicPlayer/index.tsx
+++ src/pages/MusicPlayer/index.tsx
@@ -1,5 +1,5 @@
-import { IonButton, IonButtons, IonIcon, IonItem, IonLabel, IonModal, IonThumbnail, IonTitle, IonToolbar } from '@ionic/react';
-import { arrowDown, folderSharp } from 'ionicons/icons';
+import { IonButton, IonButtons, IonContent, IonFooter, IonIcon, IonItem, IonLabel, IonModal, IonProgressBar, IonText, IonThumbnail, IonTitle, IonToolbar } from '@ionic/react';
+import { arrowDown, folderSharp, play, playCircle, playForward, playSkipBack, playSkipForward, repeat, stopwatch, timer } from 'ionicons/icons';
 import React from 'react';
 import './index.css';
 
@@ -11,11 +11,14 @@ const MusicPlayer: React.FC = () => {
     <>
       <IonItem className="MusicPlayer" onClick={() => setShowPlayer(true)}>
         <IonThumbnail slot="start" >
-          <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" alt="" />
+          <img src="\assets\image\kilarov-zaneit-ZRFztIxiy3M-unsplash.jpg" alt="" />
         </IonThumbnail>
         <IonLabel>Item Thumbnail</IonLabel>
       </IonItem>
       <IonModal isOpen={showPlayer} >
+        <div className="backimage">
+          <img src="\assets\image\kilarov-zaneit-ZRFztIxiy3M-unsplash.jpg" alt="" />
+        </div>
         <IonToolbar>
           <IonButtons slot="start">
             <IonButton onClick={() => setShowPlayer(false)}>
@@ -29,6 +32,19 @@ const MusicPlayer: React.FC = () => {
             </IonButton>
           </IonButtons>
         </IonToolbar>
+        <IonContent>
+          <IonText>TEST</IonText>
+        </IonContent>
+        <IonFooter>
+          <IonProgressBar value={0.5}></IonProgressBar>
+          <IonButtons>
+            <IonButton><IonIcon icon={stopwatch} /></IonButton>
+            <IonButton><IonIcon icon={playSkipBack} /></IonButton>
+            <IonButton><IonIcon icon={playCircle} /></IonButton>
+            <IonButton><IonIcon icon={playSkipForward} /></IonButton>
+            <IonButton><IonIcon icon={repeat} /></IonButton>
+          </IonButtons>
+        </IonFooter>
       </IonModal>
     </>
   );
