c704ca10c2c068bcd73271f3985a8b53e6b8b6ed
evaVDE <eva.vderve@gmail.com>
2020-11-12 16:02:57 +0100

    tous les moods

--- src/pages/ThemeList/index.tsx
+++ src/pages/ThemeList/index.tsx
@@ -17,24 +17,57 @@ const ThemeList: React.FC<ThemeListProps> = ({ themes, router }) => {
           <IonButtons slot="start">
             <IonBackButton />
           </IonButtons>
-          <IonTitle>Tous les moods</IonTitle>
+          <IonTitle>TOUS LES MOODS</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <div className="themes">
           <div className="theme">
             <IonButton expand="block">
-              <IonImg src="/assets/image/kilarov-zaneit-ZRFztIxiy3M-unsplash.jpg" />
+              <div className="themesensuel">
+                <p>Sensuel</p>
+              </div>
+                <IonImg src="/assets/image/red.jpg" />
+            </IonButton>
+            </div>
+          <div className="theme">
+            <IonButton expand="block">
+            <div className="themerebelle">
+                <p>Rebelle</p>
+              </div>
+              <IonImg src="/assets/image/CourtneyFireCloseup.jpg" />
             </IonButton>
           </div>
           <div className="theme">
             <IonButton expand="block">
-              <IonImg src="/assets/image/kilarov-zaneit-ZRFztIxiy3M-unsplash.jpg" />
+            <div className="themereve">
+                <p>Rêveur</p>
+              </div>
+              <IonImg src="/assets/image/cloudblue.jpg" />
             </IonButton>
           </div>
           <div className="theme">
             <IonButton expand="block">
-              <IonImg src="/assets/image/kilarov-zaneit-ZRFztIxiy3M-unsplash.jpg" />
+            <div className="themejoie">
+                <p>Joyeux</p>
+              </div>
+              <IonImg src="/assets/image/yellowballon.jpg" />
+            </IonButton>
+          </div>
+          <div className="theme">
+            <IonButton expand="block">
+            <div className="thememelan">
+                <p>Mélancolie</p>
+              </div>
+              <IonImg src="/assets/image/darkblue.jpg" />
+            </IonButton>
+          </div>
+          <div className="theme">
+            <IonButton expand="block">
+            <div className="themefrenesie">
+                <p>Frénétique</p>
+              </div>
+              <IonImg src="/assets/image/\Frénétique.jpg" />
             </IonButton>
           </div>
         </div>
